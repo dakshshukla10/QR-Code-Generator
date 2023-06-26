97bf66b7d10933a668970336d27c10b93b61fb8e
dakshshukla10 <123104147+dakshshukla10@users.noreply.github.com>
2023-06-26 23:48:15 +0530

    GUI added(not functional yet)

--- QR_Code_Gen.py
+++ QR_Code_Gen.py
@@ -1,1 +1,13 @@
-#Initalization of the QR Code Generator
+from tkinter import*
+class QR_Code_Gen:
+  def __init__(self,root):
+    self.root=root
+    self.root.geometry("900x500+200+50")
+    self.root.title("QR Code Generator")
+    self.root.resizable(False,False)
+    title=Label(self.root,text="QR Code Generator",font=("times new roman",40),bg='#525FE1').place(x=0,y=0,relwidth=1)
+    
+    
+root=Tk()
+obj =QR_Code_Gen(root)
+root.mainloop()
