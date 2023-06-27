93e27570176c2e1f937084ba57904f0a2a7abb1b
dakshshukla10 <123104147+dakshshukla10@users.noreply.github.com>
2023-06-27 15:37:07 +0530

    Modified UI,Updated color changes using colohunt.co's color palette

--- QR_Code_Gen.py
+++ QR_Code_Gen.py
@@ -1,13 +1,22 @@
+#importing libraries
 from tkinter import*
 class QR_Code_Gen:
   def __init__(self,root):
     self.root=root
-    self.root.geometry("900x500+200+50")
-    self.root.title("QR Code Generator")
-    self.root.resizable(False,False)
-    title=Label(self.root,text="QR Code Generator",font=("times new roman",40),bg='#525FE1').place(x=0,y=0,relwidth=1)
+    self.root.configure(bg='#222831') #Setting background color
+    self.root.geometry("900x500+200+50") #Setting window size
+    self.root.title("QR Code Generator") #Setting window title
+    self.root.resizable(False,False) #Setting window unresizable
     
+    title=Label(self.root,text="QR Code Generator",font=("Drugsther",40),bg='#222831',fg='#EEEEEE').place(x=0,y=0,relwidth=1)
     
-root=Tk()
-obj =QR_Code_Gen(root)
-root.mainloop()
+    #User Detail Window
+    user_Frame=Frame(self.root,bd=2,relief=RIDGE,bg='#393E46')
+    user_Frame.place(x=50,y=100,width=500,height=380)
+    
+    user_title=Label(user_Frame,text="USER DETAILS",font=("Drugsther",30),bg="#393E46",fg='#EEEEEE').place(x=0,y=0,relwidth=1)
+        
+    
+root=Tk() #Creating object of Tk class
+obj =QR_Code_Gen(root) #Creating object of the class and passing root as argument 
+root.mainloop() #Keeps the window open until we close it
